40359ceedd301286909e623b0288aa5ba6e760d3
bhavitha87 <86643898+bhavitha87@users.noreply.github.com>
2026-03-20 15:14:19 -0600

    Update partb.c

--- partb.c
+++ partb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     char first_name[15];
@@ -14,6 +15,51 @@ typedef struct {
 
 Student students[100];
 int student_count = 0;
+// Function declaration
+void add_student();
+int delete_student();
+void display_students();
+int isValidDate(int, int, int);
+int isLeap(int);
+
+void main()
+{
+   int choice;
+   do
+   {
+      printf("\n1. Add Student");
+      printf("\n2. View Student");
+      printf("\n3. Delete Student");
+      printf("\n4. Exit");
+      printf("\nEnter your choice:");
+      scanf("%d", &choice);
+
+      // Contol flow statement to execute based on choice
+      switch (choice)
+      {
+      case 1:
+         add_student();
+         break;
+      case 2:
+         display_students();
+         break;
+      case 3:
+         if (delete_student() == 0)
+         {
+            printf("Invalid student Id. Please check it again");
+         }
+         else
+         {
+            printf("\nDeleted student succesfully.");
+         }
+         break;
+      case 4:
+         break;
+      default:
+         printf("Invalid entry");
+      }
+   } while (choice != 4);
+}
 
 void add_student() {
     if (student_count >= 100) {
@@ -49,4 +95,62 @@ void add_student() {
 
     students[student_count++] = new_student;
     printf("Student added successfully.\n");
+}
+// Function for deleting student from array
+int delete_student()
+{
+   int i, index = -1, student_id;
+   printf("\nEnter student id to be deleted:");
+   scanf("%d", &student_id);
+   for (i = 0; i <= student_count; i++)
+   {
+      // Stop searching the elements as soon it found id in array
+      if (students[i].id == student_id)
+      {
+         index = i;
+         i = student_count;
+         break;
+      }
+   }
+
+   if (index == -1)
+   {
+      return 0;
+   }
+   else
+   {
+      // Shift the next element to current location.
+      for (i = index; i < student_count; i++)
+      {
+         if ((i + 1) <= student_count)
+         {
+            students[i] = students[i + 1];
+         }
+      }
+
+      student_count--;
+      return 1;
+   }
+}
+
+// Function for displaying all students in student array
+void display_students()
+{
+   int i;
+   if (student_count == -1)
+   {
+      printf("Student list is empty. Please add new student and try this option.");
+   }
+   for (i = 0; i <= student_count; i++)
+   {
+      printf("\nStudent Id is:%d", students[i].id);
+      printf("\nStudent First Name is:%s", students[i].first_name);
+      printf("\nStudent last name is:%s", students[i].first_name);
+      printf("\nStudent Date of birth is:%s", students[i].dob);
+      printf("\nStudent gender is:%s", students[i].gender);
+      printf("\nStudent GPA is:%f", students[i].gpa);
+      printf("\nStudent current semester is:%d", students[i].semester);
+      printf("\nStudent program is:%s", students[i].program);
+      printf("\nNumber of courses are:%d", students[i].num_courses);
+   }
 }
